7fb41a06e07b6786df68b0f335567e3334b81075
garciaAlvarezPelayo <xxerpelaxx@gmail.com>
2020-02-15 18:12:13 +0100

    SDI-SpringBoot- 6 - Validación de datos en el servidor

--- sdi1920-605-lab-spring/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi1920-605-lab-spring/src/main/java/com/uniovi/controllers/UsersController.java
@@ -5,10 +5,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.entities.*;
 import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
+import com.uniovi.validators.SignUpFormValidator;
 
 @Controller
 public class UsersController {
@@ -16,6 +19,8 @@ public class UsersController {
 	private UsersService usersService;
 	@Autowired
 	private SecurityService securityService;
+	@Autowired
+	private SignUpFormValidator signUpFormValidator;
 
 	@RequestMapping("/user/list")
 	public String getListado(Model model) {
@@ -62,12 +67,18 @@ public class UsersController {
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
-	public String signup() {
+	public String signup(Model model) {
+		model.addAttribute("user", new User());
 		return "users/signup";
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String signup(@ModelAttribute("user") User user, Model model) {
+	public String signup(@Validated User user, BindingResult result) {
+		signUpFormValidator.validate(user, result);
+		if(result.hasErrors()) {
+			return "users/signup";
+		}
+		
 		usersService.addUser(user);
 		securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
 		return "redirect:home";
@@ -80,10 +91,10 @@ public class UsersController {
 
 	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
 	public String home(Model model) {
-	 Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-	 String dni = auth.getName();
-	 User activeUser = usersService.getUserByDni(dni);
-	 model.addAttribute("markList", activeUser.getMarks());
-	 return "users/home";
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String dni = auth.getName();
+		User activeUser = usersService.getUserByDni(dni);
+		model.addAttribute("markList", activeUser.getMarks());
+		return "users/home";
 	}
 }
