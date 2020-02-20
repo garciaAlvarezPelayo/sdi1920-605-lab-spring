991527f4d01458bc69f4d5327a8a3f16a181dc3a
garciaAlvarezPelayo <xxerpelaxx@gmail.com>
2020-02-20 20:59:26 +0100

    SDI-SpringBoot-Sesión-4.1-Sesiones

--- sdi1920-605-lab-spring/src/main/java/com/uniovi/services/MarksService.java
+++ sdi1920-605-lab-spring/src/main/java/com/uniovi/services/MarksService.java
@@ -1,7 +1,11 @@
 package com.uniovi.services;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +17,8 @@ import com.uniovi.repositories.MarksRepository;
 public class MarksService {
 	@Autowired
 	private MarksRepository marksRepository;
+	@Autowired
+	private HttpSession httpSession;
 
 	public List<Mark> getMarks() {
 		List<Mark> marks = new ArrayList<Mark>();
@@ -21,7 +27,14 @@ public class MarksService {
 	}
 
 	public Mark getMark(Long id) {
-		return marksRepository.findById(id).get();
+		Set<Mark> consultedList = (Set<Mark>) httpSession.getAttribute("consultedList");
+		if (consultedList == null) {
+			consultedList = new HashSet<Mark>();
+		}
+		Mark obtainedmark = marksRepository.findById(id).get();
+		consultedList.add(obtainedmark);
+		httpSession.setAttribute("consultedList", consultedList);
+		return obtainedmark;
 	}
 
 	public void addMark(Mark mark) {
