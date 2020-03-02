2e400157237ac1dd97795d2807a782df15d58c78
uo264761 <uo264761@EII-LIBRE-03.aulasuo.uniovi.es>
2020-03-02 09:29:19 +0100

    update

--- sdi1920-605-lab-spring/src/main/java/com/uniovi/Sdi1920605LabSpringApplication.java
+++ sdi1920-605-lab-spring/src/main/java/com/uniovi/Sdi1920605LabSpringApplication.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.uniovi;
 
 import org.springframework.boot.SpringApplication;
@@ -12,18 +11,3 @@ public class Sdi1920605LabSpringApplication {
 	}
 
 }
-=======
-package com.uniovi;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Sdi1920605LabSpringApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Sdi1920605LabSpringApplication.class, args);
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/garciaAlvarezPelayo/sdi1920-605-lab-spring.git
--- sdi1920-605-lab-spring/src/test/java/com/uniovi/Sdi1920605LabSpringApplicationTests.java
+++ sdi1920-605-lab-spring/src/test/java/com/uniovi/Sdi1920605LabSpringApplicationTests.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.uniovi;
 
 import org.junit.jupiter.api.Test;
@@ -12,18 +11,3 @@ class Sdi1920605LabSpringApplicationTests {
 	}
 
 }
-=======
-package com.uniovi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Sdi1920605LabSpringApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/garciaAlvarezPelayo/sdi1920-605-lab-spring.git
